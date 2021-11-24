e22ac351d57bf5ee55f769b695ba568cb9349185
DanilDerypaska <82759878+DanilDerypaska@users.noreply.github.com>
2021-11-24 21:13:53 +0200

    first commit

---
+++ Program/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ClassLibrary;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1) Функцiя знаходження гіпотенузи прямокутного трикутника");
+            double katetOne = Enter("Введіть перший катет ");
+            double katetTwo = Enter("Введіть другий катет ");
+            double c;
+            c = Lib.FindHypotenuse(katetOne, katetTwo);
+            Console.WriteLine($"Гипотенуза = {c}");
+            Console.ReadLine();
+
+            Console.WriteLine("2) Знаходження суми ряду.");
+            Console.WriteLine("Введiть n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double x = Enter("Введiть x: ");
+            Console.WriteLine("Сума ряду" + Lib.Summa(n, x));
+     
+        }
+
+        public static double Enter(string text)
+        {
+            double n;
+            Console.Write(text);
+            while (!double.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Невірні дані");
+                Console.Write("Спробуйте ще раз : ");
+            }
+            return n;
+        }   
+    }
+}
---
+++ SuperLibrary/Lib.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Lib
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name="katetOne"></param>
+        /// <param name="katetTwo"></param>
+        /// <returns></returns>        
+        public static double FindHypotenuse(double katetOne, double katetTwo)
+        {
+
+            double koef = katetOne + katetTwo;
+            if (koef > 0)
+            {
+                return (Math.Sqrt(Math.Pow(katetOne, 2) + Math.Pow(katetTwo, 2)));
+            }
+            else
+            {
+                Console.WriteLine("Ви ввели не коректні данні");
+                return 0;
+            }
+
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="x"></param>
+        /// <returns></returns>        
+        public static double Summa(int n, double x)
+        {
+            double s = 0;
+            bool q = true;
+            for (int i = 1; i == n; i++)
+            {
+                if (q)
+                {
+                    s = s + (Math.Sin(i * x) / i);
+                    q = false;
+                }
+                else
+                {
+                    s = s - (Math.Sin(i * x) / i);
+                    q = true;
+                }
+            }
+            s = s + Math.Pow(-1, n - 1) * Math.Sin(n * x) / n;
+            return s;
+        }
+
+    }
+}
